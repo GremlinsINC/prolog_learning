f536b66b63a8a965ff700795e3c49a37b3e37f95
Daniil Gerasimov <giesssoochyt@yandex.ru>
2025-09-17 20:18:53 +0300

    Fist prolog lab

---
+++ v1.pl
@@ -0,0 +1,30 @@
+inv(0,1).
+inv(1,0).
+
+mult(0,0,1).
+mult(0,1,1).
+mult(1,0,1).
+mult(1,1,0).
+
+
+solve(X1, X2, X3, X4, Result) :-
+    inv(X1, Y1),
+    mult(X2, X3, Y2),
+    mult(X3, X4, Y3),
+    inv(Y3, Y4),
+    mult(Y1, Y2, Y5),
+    mult(Y4, Y5, Result),
+    format("Y=~w: X1=~w, X2=~w, X3=~w, X4=~w~n", [Result, X1, X2, X3, X4]).
+
+main :-
+    write('Введите значения (0 или 1):'), nl,
+    write('X1 -> '), read(X1),
+    write('X2 -> '), read(X2),
+    write('X3 -> '), read(X3),
+    write('X4 -> '), read(X4),
+    nl,
+    solve(X1, X2, X3, X4, _),
+    fail.
+main.
+
+:- initialization(main).
---
+++ v3.pl
@@ -0,0 +1,31 @@
+inv(0,1).
+inv(1,0).
+
+mult(0,0,1).
+mult(0,1,1).
+mult(1,0,1).
+mult(1,1,0).
+
+
+solve(X1, X2, X3, X4, Result) :-
+    inv(X1, Y1),
+    mult(X2, X3, Y2),
+    mult(X3, X4, Y3),
+    inv(Y3, Y4),
+    mult(Y1, Y2, Y5),
+    mult(Y4, Y5, Result),
+    format("Y=~w: X1=~w, X2=~w, X3=~w, X4=~w~n", [Result, X1, X2, X3, X4]).
+
+main :-
+    write('Введите значения (0 или 1):'), nl,
+    write('Result -> '), read(Result),
+    nl,
+    member(X1, [0,1]),
+    member(X2, [0,1]),
+    member(X3, [0,1]),
+    member(X4, [0,1]),
+    solve(X1, X2, X3, X4, Result),
+    fail.
+main.
+
+:- initialization(main).
